965aba44e04463e9b7958c9b081bf432b29d5ca7
PabloRP <pablo.mg@hotmail.com>
2017-07-04 20:21:53 -0300

    delete config conection bd

--- bd/configLocal.php
+++
@@ -1,8 +0,0 @@
-<?php
-	define('DB_HOSTNAME', 'localhost');
-	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', null);
-	define('DB_DATABASE','todolist');
-	define('DB_PREFIX','cw');
-	define('DB_CHARSET','utf8');
-?>
